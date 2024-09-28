f14d6d7dede1726e55dba61ca8b9993fee67ea4f
miloa2002 <agudelo.tapias@gmail.com>
2024-09-27 19:39:23 -0500

    Feat: Filter items

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref } from "vue";
 import searchTask from "./components/SearchTask.vue"
 import Modal from "./components/Modal.vue"
 
-const categories = ref(['ALL','PERSONAL','HOME','BUSINESS'])
+const categories = ref(['ALL', 'PERSONAL', 'HOME', 'BUSINESS'])
 
 const modal = ref(false)
 
@@ -15,38 +15,33 @@ const deleteModal = () => {
   modal.value = false;
 }
 
+const selectedCategory = ref("ALL")
+
+const selectMenu = (item) => {
+  return selectedCategory.value = item
+}
 </script>
 
 <template>
-
-  <main class= " h-screen w-full bg-gray-200">
+  <main class=" h-screen w-full bg-gray-200">
     <searchTask @show-modal="showModal" />
-
-
     <div class="container mx-auto">
       <h2 class="font-bold text-2xl mt-3">Your Notes</h2>
-
       <div>
-
         <div>
           <ul class="flex gap-2  text-sm font-medium border-b-2 max-w-min border-slate-600  ">
             <li 
-            v-for="(item, index) in categories" 
-            :key="index"
-            class=" cursor-pointer">{{ item }}</li>
-           
+              v-for="(item, index) in categories" 
+              :key="index" class="cursor-pointer"
+              @click="selectMenu(item)"
+              :class="[selectedCategory === item ? 'text-cyan-600' : 'text-gray-400']"
+            >{{ item }}</li>
           </ul>
-
         </div>
-
         <div>
-
         </div>
-
       </div>
-
     </div>
-
   </main>
   <Modal :modal="modal" @delete-modal="deleteModal" />
 </template>
--- src/components/SearchTask.vue
+++ src/components/SearchTask.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 defineEmits(["show-modal"])
 
 </script>
