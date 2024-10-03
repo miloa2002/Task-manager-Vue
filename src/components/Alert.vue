c10957e4b58c279ff8c46c180fc58f2d11e03178
miloa2002 <agudelo.tapias@gmail.com>
2024-10-02 21:07:35 -0500

    FEAT: Lógica crud

---
+++ src/components/Alert.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg-red-600 p-2 rounded-md">
+
+        <slot></slot>
+    </div>
+</template>
--- src/components/SearchTask.vue
+++ src/components/SearchTask.vue
@@ -1,12 +1,24 @@
 <script setup>
-defineEmits(["show-modal"])
+import { ref } from 'vue';
+
+defineEmits(["show-modal", "search-task-value"])
+
+const props = defineProps({
+    task: {
+        type: Object,
+        required: true
+    }
+})
+
+const searchText = ref("")
+
 
 </script>
 
 <template>
     <div class=" bg-white p-3 shadow-md">
         <div class="container mx-auto  flex items-center gap-4">
-            <input placeholder="Buscar Tarea..." type="text" class=" w-full border-cyan-500 border outline-none rounded-md p-2 text-gray-500">
+            <input @input="$emit('search-task-value', $event.target.value)" :value="searchText" placeholder="Buscar Tarea..." type="text" class=" w-full border-cyan-500 border outline-none rounded-md p-2 text-gray-500">
             <div class="cursor-pointer flex justify-center items-center gap-2 bg-cyan-400 p-2 text-white rounded-full" @click="$emit('show-modal')">
                 <span>+</span>
                 <button>Add</button>
